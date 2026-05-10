c00f626c87eb0ca7da53c5991f9b4c4d9f9d5ca4
Tahira <taha.zuka@gmail.com>
2026-05-10 11:49:03 +0200

    routes_a: implementiran PATCH endpoint

--- routes_a.py
+++ routes_a.py
@@ -51,3 +51,16 @@ def update_album(album_id: int, album_update: AlbumCreate, session: Session=Depe
     session.commit()
     session.refresh(db_album)
     return db_album
+
+@router.patch("/{album_id}", response_model=Album)
+def partial_update_album(album_id: int, album_update: AlbumUpdate, session: Session=Depends(get_session)):
+    db_album = session.get(Album, album_id)
+    if not db_album:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Album nije pronađen")
+    album_data = album_update.model_dump(exclude_unset=True) 
+    for key, value in album_data.items():
+        setattr(db_album, key, value)
+    session.add(db_album)
+    session.commit()
+    session.refresh(db_album)
+    return db_album
